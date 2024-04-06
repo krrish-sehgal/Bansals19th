7699dadb85110f25b5c8b3948fbb9e7c1fc4e829
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2024-04-06 09:51:13 +0530

    first heroku commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const path = require('path');
-
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const https = require('https');
+
+const helmet = require('helmet');
+const compression = require('compression');
 
 const app = express();
 
@@ -11,13 +15,18 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin.js');
 const errorController = require('./controllers/error.js')
 
+// app.use(helmet());
+app.use(compression());
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(adminRoutes);
 
 app.use(errorController.get404);
-const PORT = process.env.PORT || 3000; // Use process.env.PORT for production environment
+const PORT = process.env.PORT || 3000; 
+
+// https.createServer({key:privateKey , cert:certificate } , app)
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
