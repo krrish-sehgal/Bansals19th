b773536a61459c0c167891268d518ab0240176ce
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2024-04-07 12:24:04 +0530

    vijsmessgedone

--- public/bansalCard/script2.js
+++ public/bansalCard/script2.js
@@ -3,7 +3,7 @@
   var messages = {
     krrish: ['Maza aarha website par fraud insaan?, meri hatho ki mail hai ye , sambhal ke rakhiyo', 'If you\'re hot , sorry that statement is wrong , theres no if','10 rupay ki pepsi, meri bansal secccccyy','istg ,you\'re unbilieavably awesomeee','bansal , teri jaisi ladki flirt karne ke liye nhi bani , but mai konsa bunny hu ','tujhe dekha toh ye jana sanam ','you matter , aight , see the code if you dont believe its fuckin big','you know what you are ? cute, thats right','sur se lekar paer tujhe happy wala birthday meri jaan',`bet you thought, this was the only gift you were getting:https://open.spotify.com/playlist/2A3hulMd1oJNOvLk3oeE55?si=X9A2Nk4dTtSXUjcX0LRZJg&pi=a-IZ-S_5SkTJeg"`,],
     
-    vij: ['Happy birthday dukhi aatma','chamakti raahiyo humesha'],
+    vij: ['9 years ive known you and you\'re still the most beautiful soul ive ever met','ALWAYS THE SILLIEST DUO IN THE ROOM W COUNTLESS INSIDE JOKES HEHE','i think it wasnt love that kept us bonded for years but it was def my greed for ritu aunty’s breath taking mind blasting khaana(specially grilled sandwiches).','bitching about the whole wold then saying saanu ki is our fav part time hobby','tere liye bande bulwa dungi by god if anyone ever tries to hurt my pasandida aurat ever','tere emo era se leke tera lover era sab dekha hai maine and istg i love you for making me experience what a real female friendship is.','from being a girl w just the same name to being the first one to know about every detail of my life, ive been in awe of you ever since.','from being a girl w just the same name to being the first one to know about every detail of my life, ive been in awe of you ever since.','agar itni smoothly kisi ladke ke saath flirt karliya na jitna tere saath karti hu by god 5-6 bande hote mere abhi tak.','youve always been there for me no matter what and i will always feel like home w you.','the first of birthdays when we arent there w each other but im sending you loads of hugs and kisses all the way from 7 samundar paar. i miss you sm.','and wo sab toh theek hai lekin MATERIAL TOH KHAALE MAATE hehe.','i love you sm bkl always keep smiling and apun idharich hai so tension nai lene ka re'],
     
     shreya:['Happy birthday magnificent human! Wishing you endless laughter and joy', 'No distance can ever break the bond we share as friends. :)', 'I hope to see u this year bishhhh( Teri sau 😁)', 'Remember the time we broke the tennis table during our play rehersals,Bansal, and I mean it, will you break more tables with me?💀', 'May your day be as bright and joyful as your future, and as wild and crazyas your,last Google search history!', 'This is the last year of your teenage....have a crazyy yearr','love you <3'],
     
@@ -11,7 +11,7 @@
     
     neer:['love you','Happy Birthday','you deserve all the happiness in the world' ],
     
-    suhani:['Happy Birhtday','You are the best','Have a blast','Cheers'],
+    suhani:['Happy Birhtday','15 years & to ♾️!!','Gonna do all sorts of dumb stuff and come back to you always!! 💕','Tere pass koi option bhi nhi hai mujhe jhelne ke alawa, so nvm, love youu!! 🧿'],
     
     tejas:['Perfect example of cherry on life', 'Pta nhi kya ache karam kare the jo tu milli', 'When life gives you birthday cake, smash it on your face', 'Another year of giving life advised to random people', 'Still mother is mothering'],
     
